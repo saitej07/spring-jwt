feb66767de92a0c60ae15cc6677eeca144ce4fd7
saitej07 <saitejabollabathini@gmail.com>
2024-11-16 19:19:16 +0530

    added Kafka related classes and dependencies..

--- src/main/java/com/saiteja/spring_jwt/Controller/UserController.java
+++ src/main/java/com/saiteja/spring_jwt/Controller/UserController.java
@@ -1,6 +1,7 @@
 package com.saiteja.spring_jwt.Controller;
 
 import com.saiteja.spring_jwt.entity.User;
+import com.saiteja.spring_jwt.service.KafkaMessagePublisher;
 import com.saiteja.spring_jwt.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,13 +14,18 @@ public class UserController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private KafkaMessagePublisher publisher;
+
     @PostMapping("/register")
     public User addUser(@RequestBody User user) {
+        publisher.messageSendToTopic("User Registered: " + user.getUsername());
         return service.addUser(user);
     }
 
     @PostMapping("/login")
     public String login(@RequestBody User user) {
+        publisher.messageSendToTopic("User logged in: " + user.getUsername());
         return service.verify(user);
     }
 }
